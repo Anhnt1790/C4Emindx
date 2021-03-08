29d0b8f2daf498706ea2382638f7272fbfbb6377
anhnt1790 <anhnt1790@gmail.com>
2021-03-08 21:05:06 +0700

    a

---
+++ session4/index.js
@@ -0,0 +1,18 @@
+// let person = ['Nam', 19, 'Ha Noi', 25, false];
+// console.log(person);
+
+// Init
+// let person = {};
+// console.log(person);
+
+// let person = {
+//     name: 'Nam'
+// };
+// console.log(person);
+
+let person = {
+    name: 'Nam',
+    age: 19,
+    
+};
+console.log(person)
